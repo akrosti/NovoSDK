f7087cf592128bf731eece259931fce807dbb4ee
Joshua García Obando <Joshua.Garcia@Akros.tech>
2019-08-26 17:16:54 -0600

    actualización de métodos del SDK

--- src/android/novosdk.java
+++ src/android/novosdk.java
@@ -7,7 +7,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.support.v4.app.FragmentManager;
 import com.novopayment.tokenizationlib.Dominian.Model.Configuration.DataConfiguration;
+import com.novopayment.tokenizationlib.Dominian.Model.Configuration.UserInfo;
 import com.novopayment.tokenizationlib.Dominian.Model.ResponseTokenization;
 import com.novopayment.tokenizationlib.TokenizationVisa;
 import com.novopayment.tokenizationlib.TokenizationVisaCallback;
@@ -16,16 +18,49 @@ public class novosdk extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        
         if (action.equals("test")) {
             String message = args.getString(0);
             this.test(message, callbackContext);
             return true;
         }
+
         if (action.equals("enrollDeviceVisa")) {
             String message = args.getString(0);
             this.enrollDeviceVisa(message, callbackContext);
             return true;
         }
+
+        if (action.equals("enrollCardVisa")) {
+            String message = args.getString(0);
+            this.enrollCardVisa(message, callbackContext);
+            return true;
+        }
+
+        if (action.equals("getContentCard")) {
+            String message = args.getString(0);
+            this.getContentCard(message, callbackContext);
+            return true;
+        }
+
+        if (action.equals("lifecycleManagerTokenVisa")) {
+            String message = args.getString(0);
+            this.lifecycleManagerTokenVisa(message, callbackContext);
+            return true;
+        }
+
+        if (action.equals("selectCardVisa")) {
+            String message = args.getString(0);
+            this.selectCardVisa(message, callbackContext);
+            return true;
+        }
+
+        if (action.equals("getTransactionHistory")) {
+            String message = args.getString(0);
+            this.getTransactionHistory(message, callbackContext);
+            return true;
+        }
+
         return false;
     }
 
@@ -40,19 +75,142 @@ public class novosdk extends CordovaPlugin {
 
     private void enrollDeviceVisa(String message, CallbackContext callbackContext) {
         
-        DataConfiguration dataConfiguration = new DataConfiguration("", null, null, null, null);
+        try {
 
-        TokenizationVisa tokenizationVisa = TokenizationVisa.INSTANCE;
-        tokenizationVisa.enrollDeviceVisa(this.cordova.getActivity(), dataConfiguration, new TokenizationVisaCallback.VTSCallback() {
-            @Override
-            public void onSuccessResponse(ResponseTokenization responseTokenization) {
-                callbackContext.success(responseTokenization.toString());
-            }
+            DataConfiguration dataConfiguration = new DataConfiguration("", null, null, null, null);
 
-            @Override
-            public void onFailedResponse(ResponseTokenization responseTokenization) {
-                callbackContext.error(responseTokenization.toString());
-            }
-        }); 
+            TokenizationVisa tokenizationVisa = TokenizationVisa.INSTANCE;
+            tokenizationVisa.enrollDeviceVisa(this.cordova.getActivity(), dataConfiguration, new TokenizationVisaCallback.VTSCallback() {
+                @Override
+                public void onSuccessResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.success(responseTokenization.toString());
+                }
+
+                @Override
+                public void onFailedResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.error(responseTokenization.toString());
+                }
+            }); 
+        } catch (Exception ex) {
+            callbackContext.error("enrollDeviceVisa=> " + ex);
+        }        
+    }
+
+    private void enrollCardVisa(String message, CallbackContext callbackContext) {
+        
+        try {
+
+            DataConfiguration dataConfiguration = new DataConfiguration("", null, null, null, null);
+
+            TokenizationVisa tokenizationVisa = TokenizationVisa.INSTANCE;
+            tokenizationVisa.enrollCardVisa(this.cordova.getActivity(), dataConfiguration, new TokenizationVisaCallback.VTSCallback() {
+                @Override
+                public void onSuccessResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.success(responseTokenization.toString());
+                }
+
+                @Override
+                public void onFailedResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.error(responseTokenization.toString());
+                }
+            });
+        } catch (Exception ex) {
+            callbackContext.error("enrollCardVisa=> " + ex);
+        } 
+    }
+
+    private void getContentCard(String message, CallbackContext callbackContext) {
+
+        try {
+
+            DataConfiguration dataConfiguration = new DataConfiguration("", null, null, null, null);
+            String requiredContent = "";
+
+            TokenizationVisa tokenizationVisa = TokenizationVisa.INSTANCE;
+            tokenizationVisa.getContentCard(this.cordova.getActivity(), dataConfiguration, requiredContent, new TokenizationVisaCallback.VTSCallback() {
+                @Override
+                public void onSuccessResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.success(responseTokenization.toString());
+                }
+
+                @Override
+                public void onFailedResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.error(responseTokenization.toString());
+                }
+            });
+        } catch (Exception ex) {
+            callbackContext.error("getContentCard=> " + ex);
+        } 
+    }
+
+    private void lifecycleManagerTokenVisa(String message, CallbackContext callbackContext) {
+
+        try {
+
+            DataConfiguration dataConfiguration = new DataConfiguration("", null, null, null, null);
+
+            TokenizationVisa tokenizationVisa = TokenizationVisa.INSTANCE;
+            tokenizationVisa.lifecycleManagerTokenVisa(this.cordova.getActivity(), dataConfiguration, new TokenizationVisaCallback.VTSCallback() {
+                @Override
+                public void onSuccessResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.success(responseTokenization.toString());
+                }
+
+                @Override
+                public void onFailedResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.error(responseTokenization.toString());
+                }
+            }); 
+        } catch (Exception ex) {
+            callbackContext.error("lifecycleManagerTokenVisa=> " + ex);
+        } 
+    }
+
+    private void selectCardVisa(String message, CallbackContext callbackContext) {
+
+        try {            
+
+            DataConfiguration dataConfiguration = new DataConfiguration("", null, null, null, null);
+            int position = 0;
+            FragmentManager supportFragmentManager= null;
+
+            TokenizationVisa tokenizationVisa = TokenizationVisa.INSTANCE;
+            tokenizationVisa.selectCardVisa(this.cordova.getActivity(), dataConfiguration, position, supportFragmentManager, new TokenizationVisaCallback.VTSCallback() {
+                @Override
+                public void onSuccessResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.success(responseTokenization.toString());
+                }
+
+                @Override
+                public void onFailedResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.error(responseTokenization.toString());
+                }
+            }); 
+        } catch (Exception ex) {
+            callbackContext.error("selectCardVisa=> " + ex);
+        } 
+    }
+
+    private void getTransactionHistory(String message, CallbackContext callbackContext) {
+
+        try {
+
+            DataConfiguration dataConfiguration = new DataConfiguration("", null, null, null, null);
+
+            TokenizationVisa tokenizationVisa = TokenizationVisa.INSTANCE;
+            tokenizationVisa.getTransactionHistory(this.cordova.getActivity(), dataConfiguration, new TokenizationVisaCallback.VTSCallback() {
+                @Override
+                public void onSuccessResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.success(responseTokenization.toString());
+                }
+
+                @Override
+                public void onFailedResponse(ResponseTokenization responseTokenization) {
+                    callbackContext.error(responseTokenization.toString());
+                }
+            });
+        } catch (Exception ex) {
+            callbackContext.error("getTransactionHistory=> " + ex);
+        } 
     }
 }
--- www/novosdk.js
+++ www/novosdk.js
@@ -7,3 +7,23 @@ module.exports.test = function (arg0, success, error) {
 module.exports.enrollDeviceVisa = function (arg0, success, error) {
     exec(success, error, 'novosdk', 'enrollDeviceVisa', [arg0]);
 };
+
+module.exports.enrollCardVisa = function (arg0, success, error) {
+    exec(success, error, 'novosdk', 'enrollCardVisa', [arg0]);
+};
+
+module.exports.getContentCard = function (arg0, success, error) {
+    exec(success, error, 'novosdk', 'getContentCard', [arg0]);
+};
+
+module.exports.lifecycleManagerTokenVisa = function (arg0, success, error) {
+    exec(success, error, 'novosdk', 'lifecycleManagerTokenVisa', [arg0]);
+};
+
+module.exports.selectCardVisa = function (arg0, success, error) {
+    exec(success, error, 'novosdk', 'selectCardVisa', [arg0]);
+};
+
+module.exports.getTransactionHistory = function (arg0, success, error) {
+    exec(success, error, 'novosdk', 'getTransactionHistory', [arg0]);
+};
